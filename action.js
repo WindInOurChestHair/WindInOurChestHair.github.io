8b0fdbeda1b0f345d01439a75feb255b6ba1532a
starkizard <prabalkalia18bcs@iiitkottayam.ac.in>
2021-04-04 18:03:55 +0530

    fartboard on the wae

---
+++ action.js
@@ -0,0 +1,18 @@
+console.log("bad time");
+function playsound(e) {
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const key = document.querySelector('div[data-key="'+e.keyCode+'"]'); //just two ways of writing
+  if (!audio) return;
+  audio.currentTime = 0;
+  audio.play();
+  key.classList.add('playing');
+}
+
+function removeTransition(e) {
+  if (e.propertyName !== 'transform') return;
+  this.classList.remove('playing');
+}
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+window.addEventListener('keydown', playsound);
